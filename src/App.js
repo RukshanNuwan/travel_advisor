76d58aadab3f59f113c325e8d380071dc86c7ff8
RukshanNuwan <shanwijendra.dev@gmail.com>
2023-05-31 18:41:06 +0530

    dump

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import { CssBaseline, Grid } from '@mui/material';
+import {useEffect, useState} from "react";
+import {CssBaseline, Grid} from '@mui/material';
 
 import './App.css';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
+import {getPlacesData} from "./api";
 
-function App() {
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({lat: 0, lng: 0});
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+      setCoordinates({lat: latitude, lng: longitude});
+    })
+  }, [])
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPlacesData(bounds.sw, bounds.ne)
+      .then((data) => {
+        setPlaces(data);
+        setIsLoading(false);
+      });
+  }, [coordinates, bounds])
+
   return (
     <>
       <CssBaseline>
-        <Header />
+        <Header/>
 
-        <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid container spacing={3} style={{width: '100%'}}>
           <Grid item xs={12} md={4}>
-            <List />
+            <List places={places} childClicked={childClicked} isLoading={isLoading}/>
           </Grid>
 
           <Grid item xs={12} md={4}>
-            <Map />
+            <Map
+              coordinates={coordinates}
+              setCoordinates={setCoordinates}
+              setBounds={setBounds}
+              places={places}
+              setChildClicked={setChildClicked}
+            />
           </Grid>
         </Grid>
       </CssBaseline>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,66 @@
-const List = () => {
+import {createRef, useEffect, useState} from "react";
+import {CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, Typography} from '@mui/material';
+
+import useStyles from './styles';
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
+const List = ({places, childClicked, isLoading}) => {
+  const [type, setType] = useState('restaurant');
+  const [rating, setRating] = useState('');
+  const [eleRefs, setEleRefs] = useState([]);
+
+  const classes = useStyles();
+
+
+  useEffect(() => {
+    const refs = Array(places?.length).fill().map((_, index) => eleRefs[index] || createRef());
+
+    setEleRefs(refs);
+  }, [places])
+
   return (
-    <div>list</div>
+    <div className={classes.container}>
+      <Typography variant='h4'>Restaurants, Hotels & Attractions around you</Typography>
+
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size='5rem'/>
+        </div>
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value='restaurants'>Restaurants</MenuItem>
+              <MenuItem value='hotels'>Hotels</MenuItem>
+              <MenuItem value='attractions'>Attractions</MenuItem>
+            </Select>
+          </FormControl>
+
+          <FormControl className={classes.formControl}>
+            <InputLabel>Rating</InputLabel>
+            <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={3}>Above 3.0</MenuItem>
+              <MenuItem value={4}>Above 4.0</MenuItem>
+              <MenuItem value={4.5}>Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, index) => (
+              <Grid key={index} item xs={12}>
+                <PlaceDetails
+                  details={place}
+                  selected={Number(childClicked) === index}
+                  refProp={eleRefs[index]}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </div>
   );
 }
 
