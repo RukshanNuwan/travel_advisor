9f5fb0a7766f23fc7fc7efa32d753322ffa0b2b7
RukshanNuwan <shanwijendra.dev@gmail.com>
2023-06-01 14:49:55 +0530

    created .env

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,6 +3,7 @@ import {Paper, Rating, Typography, useMediaQuery} from '@mui/material';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
 import useStyles from './styles';
+import mapStyles from "./mapStyles";
 
 const Map = ({coordinates, setCoordinates, setBounds, places, setChildClicked, weatherData}) => {
   const classes = useStyles();
@@ -11,12 +12,12 @@ const Map = ({coordinates, setCoordinates, setBounds, places, setChildClicked, w
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{key: '###'}}
+        bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={''}
+        options={{disableDefaultUI: true, zoomControl: true, styles: mapStyles}}
         onChange={(e) => {
           setCoordinates({lat: e.center.lat, lng: e.center.lng})
           setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
